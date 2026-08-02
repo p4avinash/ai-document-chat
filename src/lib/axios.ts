8c6b80922e4a5dd4316b4680ca4548b41bfc7403
Avinash <p4avinashmaurya@gmail.com>
2026-08-02 10:18:31 +0530

    Fixing cors error

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,9 +3,6 @@ import axios from "axios"
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   timeout: 30000,
-  headers: {
-    "Content-Type": "application/json",
-  },
 })
 
 api.interceptors.request.use(
